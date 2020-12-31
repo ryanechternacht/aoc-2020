fbc97bc1f1629908930d525f959f97f3a1380661
ryan echternacht <ryan@MacBook-Pro.local>
2020-12-30 20:45:02 -0600

    day 24 pt 2

--- src/day24.clj
+++ src/day24.clj
@@ -38,3 +38,48 @@
      parse-input
      flip-tiles
      count)
+
+(defn get-neighbors [[x y]]
+  [[(inc x) (inc y)]
+   [(+ x 2) y]
+   [(inc x) (dec y)]
+   [(dec x) (dec y)]
+   [(- x 2) y]
+   [(dec x) (inc y)]])
+
+(defn get-neighbor-counts [positions]
+  (reduce (fn [acc pos]
+            (reduce (fn [acc2 pos2]
+                      (assoc acc2 pos2 (inc (get acc2 pos2 0))))
+                    acc
+                    (get-neighbors pos)))
+          {}
+          positions))
+
+(defn step-tiles [tiles]
+  (let [neighbor-counts (get-neighbor-counts tiles)
+        all-tiles (reduce conj tiles (map first neighbor-counts))]
+    (reduce (fn [acc pos]
+              (let [c (get neighbor-counts pos 0)]
+                (if (contains? tiles pos)
+                ;; currently black
+                  (if (<= 1 c 2)
+                    (conj acc pos)
+                    acc)
+                ;; currently white
+                  (if (= 2 c)
+                    (conj acc pos)
+                    acc))))
+            #{}
+            all-tiles)))
+
+(->> input
+     parse-input
+     flip-tiles
+     (#(reduce (fn [acc _]
+                 (step-tiles acc))
+               %
+               (range 100)))
+     count)
+
+(step-tiles (flip-tiles (parse-input sample)))
